8d8c66388868464e8f9867a471f86e00e28c28c8
Serhii Kermas <serhii.kermas@gmail.com>
2025-08-01 16:59:48 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 
-GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB TO 'webappuser'@'%';
-GRANT ALL ON ShopDB TO 'deploymentuser'@'%';
+GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
